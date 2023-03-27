f8f0cca357bcb338723e7b0a078fe91e980a09b8
Mattia Matteini <matteinimattia@gmail.com>
2023-03-27 11:41:09 +0200

    feat: implemented contains and store

--- src/main/scala/u04lab/code/Warehouse.scala
+++ src/main/scala/u04lab/code/Warehouse.scala
@@ -1,51 +1,85 @@
 package u04lab.code
+
 import List.*
-trait Item {
+
+trait Item:
   def code: Int
+
   def name: String
+
   def tags: List[String]
-}
+
 
 object Item:
-  def apply(code: Int, name: String, tags: List[String] = List.empty): Item = ???
+  def apply(code: Int, name: String, tags: List[String] = List.empty): Item = ItemImpl(code, name, tags)
+
+
+case class ItemImpl(override val code: Int,
+                    override val name: String,
+                    override val tags: List[String]) extends Item
+
 
 /**
  * A warehouse is a place where items are stored.
  */
-trait Warehouse {
+trait Warehouse:
   /**
    * Stores an item in the warehouse.
+   *
    * @param item the item to store
    */
   def store(item: Item): Unit
+
   /**
    * Searches for items with the given tag.
+   *
    * @param tag the tag to search for
    * @return the list of items with the given tag
    */
   def searchItems(tag: String): List[Item]
+
   /**
    * Retrieves an item from the warehouse.
+   *
    * @param code the code of the item to retrieve
    * @return the item with the given code, if present
    */
   def retrieve(code: Int): Option[Item]
+
   /**
    * Removes an item from the warehouse.
+   *
    * @param item the item to remove
    */
   def remove(item: Item): Unit
+
   /**
    * Checks if the warehouse contains an item with the given code.
+   *
    * @param itemCode the code of the item to check
    * @return true if the warehouse contains an item with the given code, false otherwise
    */
   def contains(itemCode: Int): Boolean
-}
 
-object Warehouse {
-  def apply(): Warehouse = ???
-}
+
+object Warehouse:
+  def apply(): Warehouse = WarehouseImpl()
+
+
+case class WarehouseImpl() extends Warehouse:
+
+  private var items: List[Item] = empty
+
+  override def store(item: Item): Unit = items = append(cons(item, empty), items)
+
+  override def searchItems(tag: String): List[Item] = ???
+
+  override def retrieve(code: Int): Option[Item] = ???
+
+  override def remove(item: Item): Unit = ???
+
+  override def contains(itemCode: Int): Boolean = length(filter(items)(item => item.code == itemCode)) > 0
+
 
 @main def mainWarehouse(): Unit =
   val warehouse = Warehouse()
@@ -54,24 +88,24 @@ object Warehouse {
   val dellInspiron = Item(34, "Dell Inspiron 13", cons("notebook", empty))
   val xiaomiMoped = Item(35, "Xiaomi S1", cons("moped", cons("mobility", empty)))
 
-  warehouse.contains(dellXps.code) // false
+  println(warehouse.contains(dellXps.code)) // false
   warehouse.store(dellXps) // side effect, add dell xps to the warehouse
-  warehouse.contains(dellXps.code) // true
+  println(warehouse.contains(dellXps.code)) // true
   warehouse.store(dellInspiron) // side effect, add dell inspiron to the warehouse
   warehouse.store(xiaomiMoped) // side effect, add xiaomi moped to the warehouse
-  warehouse.searchItems("mobility") // List(xiaomiMoped)
-  warehouse.searchItems("notebook") // List(dellXps, dellInspiron)
-  warehouse.retrieve(11) // None
-  warehouse.retrieve(dellXps.code) // Some(dellXps)
+  println(warehouse.searchItems("mobility")) // List(xiaomiMoped)
+  println(warehouse.searchItems("notebook")) // List(dellXps, dellInspiron)
+  println(warehouse.retrieve(11))// None
+  println(warehouse.retrieve(dellXps.code)) // Some(dellXps)
   warehouse.remove(dellXps) // side effect, remove dell xps from the warehouse
-  warehouse.retrieve(dellXps.code) // None
+  println(warehouse.retrieve(dellXps.code)) // None
 
-/** Hints:
- * - Implement the Item with a simple case class
- * - Implement the Warehouse keeping a private List of items
- * - Start implementing contains and store
- * - Implement searchItems using filter and contains
- * - Implement retrieve using find
- * - Implement remove using filter
- * - Refactor the code of Item accepting a variable number of tags (hint: use _*)
-*/
+  /** Hints:
+   * - Implement the Item with a simple case class
+   * - Implement the Warehouse keeping a private List of items
+   * - Start implementing contains and store
+   * - Implement searchItems using filter and contains
+   * - Implement retrieve using find
+   * - Implement remove using filter
+   * - Refactor the code of Item accepting a variable number of tags (hint: use _*)
+   */
